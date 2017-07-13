847ff8dde3d0829648942fdaf9db7feec448e9bc
Seb <qxssd>
2017-07-13 23:40:15 +0200

    dfdf

--- openvpn/up.sh
+++ openvpn/up.sh
@@ -7,10 +7,23 @@ echo "nameserver 80.67.169.12" | tee -a /etc/resolv.conf
 echo "nameserver 80.67.169.40" | tee -a /etc/resolv.conf
 
 #automatic IP finding
-MAIN_IP=$(ip route | awk 'NR==3{print $9}')
 VPN_IP=$4
-echo "Main IP: ${MAIN_IP}"
 echo "VPN IP: ${VPN_IP}"
+MAIN_IP=$(ip route | awk 'NR==3{print $9}')
+echo "Main IP: ${MAIN_IP}"
+GATEWAY_IP=$(ip route | awk 'NR==1{print $3}')
+echo "GATEWAYIP: ${GATEWAY_IP}"
+SUBNET=$(ip route | awk 'NR==2{print $1}')
+echo "SUBNET: ${SUBNET}"
+
+ip route add default via $route_vpn_gateway dev $1 table 200
+ip rule add from $ifconfig_local table 200
+ip route flush cache
+
+echo "-----------------------------------------------------------"
+echo "Routes: "
+ip route
+echo "-----------------------------------------------------------"
 
 . /etc/transmission/start.sh
 
