d9c3bbb20f499611fd32bddbc6169e47a18fabe8
Seb <qxssd>
2017-07-14 11:17:49 +0200

    dfdf

--- transmission/start.sh
+++ transmission/start.sh
@@ -2,6 +2,7 @@
 
 # Source our persisted env variables from container startup
 . /etc/environment.sh
+echo "Transmission home: ${TRANSMISSION_HOME}"
 
 # This script will be called with tun/tap device name as parameter 1, and local IP as parameter 4
 # See https://openvpn.net/index.php/open-source/documentation/manuals/65-openvpn-20x-manpage.html (--up cmd)
